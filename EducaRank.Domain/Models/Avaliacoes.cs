5df6155fe54509dc42d7f35c9e05c478053e70b7
davi-y08 <oisyzenx@gmail.com>
2025-10-19 09:27:16 -0300

    atualizando avaliações

--- EducaRank.Domain/Models/Avaliacoes.cs
+++ EducaRank.Domain/Models/Avaliacoes.cs
@@ -2,5 +2,12 @@
 {
     public class Avaliacoes
     {
+        public int Id { get; set; }
+        public Professor Professor { get; set; } = new Professor();
+        public int Rm_professor { get; set; }
+        public Aluno Aluno { get; set; } = new Aluno();
+        public int Rm_aluno { get; set; }
+        public int Pontuacao_alterada { get; set; }
+        public List<string> Bases { get; set; } = new List<string>();
     }
 }
