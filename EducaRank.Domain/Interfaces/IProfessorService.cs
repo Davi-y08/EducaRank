671e4af3d326363e79367ed82348d04ecc4e707c
davi-y08 <oisyzenx@gmail.com>
2025-10-21 20:47:21 -0300

    adicionando as interfaces do professor e tokens de login

--- EducaRank.Domain/Interfaces/IProfessorService.cs
+++ EducaRank.Domain/Interfaces/IProfessorService.cs
@@ -1,7 +1,18 @@
-﻿namespace EducaRank.Domain.Interfaces
+﻿using EducaRank.Domain.Models;
+
+namespace EducaRank.Domain.Interfaces
 {
     public interface IProfessorService
     {
-        
+        Task<List<Professor>> GetProfessores(); // listar professores do banco de dados do educarank
+        Task<Professor> GetProfessorById(int professor_id);
+        Task<Professor> CreateAluno(Professor professor);
+        Task<Professor> ChangePfp(string pfp);
+        Task<Professor> GetNrAvaliacoes(int professor_id);
+        Task<bool> DeleteProfessorFromEducaRank(int professor_id);
+        Task<List<Avaliacao>> GetAvaliacoesFeitas(int professor_id);
+        Task<List<Professor>> SearchProfessores(string query_str);
+        Task<Professor> ChangeEmail(string new_email);
+        Task<Professor> ChangePassword(string old_password, string new_password);
     }
 }
--- EducaRank.Domain/Interfaces/ITokenO_AuthService.cs
+++ EducaRank.Domain/Interfaces/ITokenO_AuthService.cs
@@ -1,7 +1,10 @@
-﻿namespace EducaRank.Domain.Interfaces
+﻿using EducaRank.Domain.Models;
+
+namespace EducaRank.Domain.Interfaces
 {
     public interface ITokenO_AuthService
     {
-
+        public string CreateToken_Aluno(Aluno aluno_model);
+        public string CreateToken_Professor(Professor professor_model);
     }
 }
