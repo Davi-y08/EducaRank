dfcb544ad2f2f369efb4a52b4f12a412e6f12990
davi-y08 <oisyzenx@gmail.com>
2025-10-21 23:07:41 -0300

    ..

---
+++ EducaRank.Domain/Services/AlunoService.cs
@@ -0,0 +1,16 @@
+﻿using EducaRank.Domain.Interfaces;
+
+namespace EducaRank.Domain.Services
+{
+    public class AlunoService
+    {
+        private readonly IAlunoService _alunoService;
+
+        public AlunoService(IAlunoService alunoService)
+        {
+            _alunoService = alunoService;
+        }
+
+        
+    }
+}
