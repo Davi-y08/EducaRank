fbb880ce423ddfea56f61f27a13ad0bb1b06966e
davi-y08 <oisyzenx@gmail.com>
2025-10-22 22:29:37 -0300

    aa

--- EducaRank.Domain/Services/AlunoService.cs
+++ EducaRank.Domain/Services/AlunoService.cs
@@ -1,4 +1,5 @@
 ﻿using EducaRank.Domain.Interfaces;
+using EducaRank.Domain.Models;
 
 namespace EducaRank.Domain.Services
 {
@@ -11,6 +12,32 @@ namespace EducaRank.Domain.Services
             _alunoService = alunoService;
         }
 
-        
+        public async Task<List<Aluno>> GetAlunos()
+        {
+            return await _alunoService.GetAlunos();
+        }
+
+        public async Task<Aluno> CreateAluno(Aluno aluno_model)
+        {
+            var new_aluno = new Aluno();
+            new_aluno.Curso = aluno_model.Curso;
+            new_aluno.Etec = aluno_model.Etec;
+            new_aluno.Sala = aluno_model.Sala;
+            new_aluno.Pontuacao = aluno_model.Pontuacao;
+            new_aluno.Nome = aluno_model.Nome;
+            new_aluno.Rm = aluno_model.Rm;
+            new_aluno.Idade = aluno_model.Idade;
+            new_aluno.Foto = aluno_model.Foto;
+            new_aluno.Id = aluno_model.Id;
+            new_aluno.SenhaHash = aluno_model.SenhaHash;
+            new_aluno.NrAvaliacoes = aluno_model.NrAvaliacoes;
+
+            return await _alunoService.CreateAluno(new_aluno);
+        }
+
+        public async Task<Aluno> GetAlunoById()
+        {
+
+        }
     }
 }
