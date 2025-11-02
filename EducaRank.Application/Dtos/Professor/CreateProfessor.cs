ef3d32bff862c47809f114e0e6fa5a9479798a9e
davi-y08 <oisyzenx@gmail.com>
2025-11-02 15:36:51 -0300

    Add dto professor

---
+++ EducaRank.Application/Dtos/Professor/CreateProfessor.cs
@@ -0,0 +1,15 @@
+﻿using EducaRank.Domain.Models;
+
+namespace EducaRank.Application.Dtos.Professor
+{
+    public class CreateProfessor
+    {
+        public int Rm { get; set; }
+        public string Nome { get; set; } = string.Empty;
+        public List<Materia> Materias { get; set; } = new List<Materia>();
+        public List<Sala> Salas { get; set; } = new List<Sala>();
+        public string Foto { get; set; } = string.Empty;
+        public int AvaliacoesFeitas { get; set; } = 0;
+        public string Senha { get; set; } = 
+    }
+}
