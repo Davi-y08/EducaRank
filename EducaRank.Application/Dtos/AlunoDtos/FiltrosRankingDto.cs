22a0d8ce7f05e5b37a8d1667a2b3c5920ff55a06
davi-y08 <oisyzenx@gmail.com>
2025-11-13 19:18:07 -0300

    f commit day

---
+++ EducaRank.Application/Dtos/AlunoDtos/FiltrosRankingDto.cs
@@ -0,0 +1,12 @@
+﻿namespace EducaRank.Application.Dtos.AlunoDtos
+{
+    public class FiltrosRankingDto
+    {
+        public string Etec { get; set; } = string.Empty;
+        public int? SalaId { get; set; } 
+        public int? IdadeMinima { get; set; } 
+        public int? IdadeMaxima { get; set; } 
+        public int Page { get; set; } = 1;
+        public int PageSize { get; set; } = 20;
+    }
+}
--- EducaRank_API/Controllers/AlunoController.cs
+++ EducaRank_API/Controllers/AlunoController.cs
@@ -1,5 +1,8 @@
-﻿using EducaRank.Domain.Interfaces;
+﻿using EducaRank.Application.Dtos.AlunoDtos;
+using EducaRank.Domain.Interfaces;
+using EducaRank.Infrastructure.Data;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace EducaRank_API.Controllers
 {
@@ -8,10 +11,11 @@ namespace EducaRank_API.Controllers
     public class AlunoController : ControllerBase
     {
         private readonly IAlunoService _alunoService;
-
-        public AlunoController(IAlunoService alunoService)
+        private readonly LegadoEscolaDbContext _context;
+        public AlunoController(IAlunoService alunoService, LegadoEscolaDbContext context)
         {
             _alunoService = alunoService;
+            _context = context;
         }
 
         [HttpGet("{id}")]
@@ -31,5 +35,46 @@ namespace EducaRank_API.Controllers
 
             return Ok(alunos);
         }
+
+        [HttpGet("ranking")]
+        public async Task<IActionResult> GetRankingWithFiltersAndPagination([FromQuery] FiltrosRankingDto ranking_dto) 
+        {
+            var alunos = await _alunoService.PontuacaoComFiltos(ranking_dto.Etec, 
+                ranking_dto.SalaId, 
+                ranking_dto.IdadeMinima, 
+                ranking_dto.IdadeMaxima, 
+                ranking_dto.Page, 
+                ranking_dto.PageSize);
+
+            if(alunos == null) return NotFound("Nenhum aluno encontrado"); 
+            
+            return Ok( new {
+                success = true,
+                total = alunos.Count(),
+                page = ranking_dto.Page,
+                pageSize = ranking_dto.PageSize,
+                data = alunos
+            });
+        }
+
+        //[HttpPut("mudanças")]
+        //public async Task<IActionResult> AlterarEtec()
+        //{
+        //    var alunos_legado = await _context.AlunoBdEtec.ToListAsync();
+
+        //    for (int i = 0; i < alunos_legado.Count(); i++)
+        //    {
+        //        alunos_legado[i].Etec = "Sede";
+        //    }
+
+        //    for (int i = 0; i < alunos_legado.Count(); i+=2)
+        //    {
+        //        alunos_legado[i].Etec = "Extensão";
+        //    }
+
+        //    await _context.SaveChangesAsync();
+
+        //    return Ok("Alterações concluidas com sucesso!");
+        //}
     }
 }
